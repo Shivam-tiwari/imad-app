214a41f11428c8c8fb02e15b8385dea9afb92b24
Shivam-tiwari <30958642+Shivam-tiwari@users.noreply.github.com>
2017-08-15 20:10:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,16 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/art-1.html',function(req,res){
+app.get('/art-1.html',function(req,res){
   res.sendFile(path.join(_dirname,'ui','art-1.html'));    
-});
-
-app.get('/ui/art-2.html',function(req,res){
-    res.sendFile(path.join(_dirname,'ui','art-2.html'));
-});
-
-app.get('/ui/art-3.html',function(req,res){
-    res.sendFile(path.join(_dirname,'ui','art-3.html'));
 });
 
 app.get('/ui/shivam.jpg', function (req, res) {
