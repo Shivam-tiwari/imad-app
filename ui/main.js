f624db3f4cd19c42c1c9d603cd20d339507edfbf
Shivam-tiwari <30958642+Shivam-tiwari@users.noreply.github.com>
2017-08-15 21:45:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,13 @@ element.innerHTML=`The text has been replaced by other text
                     in main.js`;
 
 var img = document.getElementById("shivam");
+var marginleft = 0;
+function MoveRight(){
+ marginleft = marginleft + 10;   
+ img.style.marginLeft = marginleft + "px";
+}
+
 img.onclick = function(){
-    img.style.marginLeft="100px";
+    var interval = setInterval(MoveRight,100);
+    
 } ;
