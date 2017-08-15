035972a96b09e0dfe371f21194a2a3d1a32bf631
Shivam-tiwari <30958642+Shivam-tiwari@users.noreply.github.com>
2017-08-15 19:48:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
+var morgan = require('morgan');//morgan is for handling o/p logs of server
+var path = require('path');//
 
 var app = express();
 app.use(morgan('combined'));
